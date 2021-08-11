ba7aefa89bbc9d6eaaa4495eb672c4574cc95516
Thiago Alcantara <brq.thiago@ao3tech.com>
2021-08-10 21:36:36 -0300

    Add projeto de teste unitario implementando classe baseTest

---
+++ src/Api.Data.Test/BaseTest.cs
@@ -0,0 +1,47 @@
+using System;
+using Api.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Api.Data.Test
+{
+    public abstract class BaseTest
+    {
+        public BaseTest()
+        {
+            
+        }
+
+        public class DbTeste : IDisposable
+        {
+            private static string dbName = $"dbApiTest_{Guid.NewGuid().ToString().Replace("-","")}";
+
+            private string connString = $"Persist Security Info=True;server=localhost;database={dbName};User=root;Password=Admin";
+            public ServiceProvider serviceProvider { get; private set; }
+
+            public DbTeste()
+            {
+                var serviceCollection = new ServiceCollection();
+                serviceCollection.AddDbContext<MyContext>(c=> 
+                c.UseMySql(connString),
+                ServiceLifetime.Transient
+                );
+
+                serviceProvider = serviceCollection.BuildServiceProvider();
+                using(var context = serviceProvider.GetService<MyContext>())
+                {
+                    context.Database.EnsureCreated();
+                }
+            }
+            
+            public void Dispose()
+            {
+                using(var context = serviceProvider.GetService<MyContext>())
+                {
+                    context.Database.EnsureDeleted();
+                }
+            }
+        }
+    }
+}
