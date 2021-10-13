7e273aea912f53e1e851cfdb99776d78a8c78966
Thiago Alcantara <thiago.prog@outlook.com>
2021-10-13 20:18:22 -0300

    finalizando implementação dos testes unitarios

--- src/Api.Data.Test/UsuarioCrudCompleto.cs
+++ src/Api.Data.Test/UsuarioCrudCompleto.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading.Tasks;
 using Api.Data.Context;
 using Api.Data.Repository;
@@ -17,7 +18,7 @@ namespace Api.Data.Test
             _serviceProvider = dbTeste.serviceProvider;
         }
 
-        [Fact(DisplayName = "CRUD cadastro de usuário")]
+        [Fact(DisplayName = "CRUD completo de usuário")]
         [Trait("CRUD", "UserEntity")]
         public async Task EPossivelRealizarCrudUsuario()
         {
@@ -31,8 +32,36 @@ namespace Api.Data.Test
                     Password = Faker.RandomNumber.Next(10, 30).ToString()
                 };
 
+                //inserindo usuario
                 var _retorno = await _repository.InsertAsync(_entity);
                 Assert.True(_retorno.Id != System.Guid.Empty);
+
+                //atualizando usuario
+                _entity.Nome = Faker.Name.First();
+                var _registroAtualizado = await _repository.UpdateAsync(_entity);
+                Assert.NotNull(_registroAtualizado);
+                Assert.Equal(_entity.Nome, _registroAtualizado.Nome);
+
+                //verifica se o usuario existe
+                var _registroExiste = await _repository.ExistAsync(_registroAtualizado.Id);
+                Assert.True(_registroExiste);
+
+                //selecionando por Id
+                var _registroSelecionado = await _repository.SelectAsync(_registroAtualizado.Id);
+                Assert.NotNull(_registroSelecionado);
+
+                //selecionando todos
+                var _todosRegistros = await _repository.SelectAllAsync();
+                Assert.NotNull(_todosRegistros);
+                Assert.True(_todosRegistros.Count() > 1);
+
+                //removendo usuario
+                var _removeRegistro = await _repository.DeletetAsync(_registroSelecionado.Id);
+                Assert.True(_removeRegistro);
+
+                //login com usuario padrao
+                var _usuarioPadrao = await _repository.FindByLogin("helpdeskprog@outlook.com");
+                Assert.NotNull(_usuarioPadrao);
             }
         }
     }
